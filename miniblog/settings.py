612b79bbc6d771cb39a9fb89a0ee397d345eca64
ujjwal-dahal <toppelp@gmail.com>
2024-11-24 20:35:22 +0545

    Database error has solved

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -62,7 +62,7 @@ WSGI_APPLICATION = 'miniblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': config('DB_NAME'),       
         'USER': config('DB_USER'),       
         'PASSWORD': config('DB_PASSWORD'),  
